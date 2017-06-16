cdbd2f850e66cccced247109f46c6316319c3d70
Michael Adler <protomikex@gmail.com>
2017-06-16 15:00:57 -0400

    added pages

--- js/custom.js
+++ js/custom.js
@@ -5,27 +5,6 @@ var $trellotask = $('#trellotasks');
 var $trellobadge = $('#trellobadge')
 var $ta = $('#ta');
 
-$.ajax({
-type:'GET', 
-dataType:'jsonp',
-crossDomain:true,
-url:'https://api.meetup.com/Code-for-FTL/photo_albums?photo-host=public&page=20&sig_id=4886483&sig=9e2ed9ed592429eef676820f0e8287de6ef5f3c1',
-success:function(results){
-
-
-$.each(results.data.slice(0,1), function(i, result){
-$items.css("background-image",'url('+ result.photo_sample[0].highres_link +')');
-
-});
-
-},
-error: function(e){
-    console.log(e);
-}
-
-
-
-});
 
 $.ajax({
 url: 'https://api.trello.com/1/boards/8TZhObPN?fields=name,desc&cards=open&card_fields=all&lists=open&list_fields=all&members=all&member_fields=all',
